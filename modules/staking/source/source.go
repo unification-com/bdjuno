8b9c1989b4ccb2da3beee99a74c10e5a6cf510dd
Magic Cat <pusz.monika@gmail.com>
2022-01-21 18:47:02 +0000

    Cleanup

--- modules/staking/source/source.go
+++ modules/staking/source/source.go
@@ -12,7 +12,6 @@ type Source interface {
 	GetDelegatorDelegations(height int64, delegator string) ([]stakingtypes.DelegationResponse, error)
 	GetDelegationsWithPagination(height int64, delegator string, pagination *query.PageRequest) (*stakingtypes.QueryDelegatorDelegationsResponse, error)
 	GetDelegatorRedelegations(height int64, delegator string, pagination *query.PageRequest) (*stakingtypes.QueryRedelegationsResponse, error)
-	GetValidatorDelegations(height int64, validator string) ([]stakingtypes.DelegationResponse, error)
 	GetPool(height int64) (stakingtypes.Pool, error)
 	GetParams(height int64) (stakingtypes.Params, error)
 	GetUnbondingDelegations(height int64, delegator string, pagination *query.PageRequest) (*stakingtypes.QueryDelegatorUnbondingDelegationsResponse, error)
