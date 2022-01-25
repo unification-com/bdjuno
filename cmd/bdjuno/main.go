749acaffc950b382e7a564c23c157ef85a818708
Magic Cat <pusz.monika@gmail.com>
2022-01-25 12:17:34 +0000

    Add Message Address Parser

--- cmd/bdjuno/main.go
+++ cmd/bdjuno/main.go
@@ -17,6 +17,10 @@ import (
 	"github.com/forbole/bdjuno/v2/database"
 	"github.com/forbole/bdjuno/v2/modules"
 	osmosisapp "github.com/osmosis-labs/osmosis/app"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	junomessages "github.com/desmos-labs/juno/modules/messages"
+	gammtypes "github.com/osmosis-labs/osmosis/x/gamm/types"
 )
 
 func main() {
@@ -62,6 +66,17 @@ func getBasicManagers() []module.BasicManager {
 // This should be edited by custom implementations if needed.
 func getAddressesParser() messages.MessageAddressesParser {
 	return messages.JoinMessageParsers(
+		osmosisMessageAddressesParser,
 		messages.CosmosMessageAddressesParser,
 	)
+}
+
+// osmosisMessageAddressesParser represents a parser able to get the addresses of the involved
+// accounts from an Osmosis message
+func osmosisMessageAddressesParser(_ codec.Marshaler, cosmosMsg sdk.Msg) ([]string, error) {
+	if msg, ok := (cosmosMsg).(*gammtypes.MsgCreatePool); ok {
+		return []string{msg.Sender, msg.FuturePoolGovernor}, nil
+	}
+
+	return nil, junomessages.MessageNotSupported(cosmosMsg)
 }
