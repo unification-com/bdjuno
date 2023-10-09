0731d4319a154a74159b14b5297058ab569ecf1f
Codegnosis <paul@codegnosis.co.uk>
2023-10-09 11:48:45 +0100

    modules - handle v1 & v1beta1 gov messages

--- modules/gov/handle_msg.go
+++ modules/gov/handle_msg.go
@@ -24,7 +24,13 @@ func (m *Module) HandleMsg(index int, msg sdk.Msg, tx *juno.Tx) error {
 
 	switch cosmosMsg := msg.(type) {
 	case *govtypesv1beta1.MsgSubmitProposal:
-		return m.handleMsgSubmitProposal(tx, index, cosmosMsg)
+		return m.handleMsgSubmitProposalV1Beta1(tx, index, cosmosMsg)
+
+	case *govtypesv1beta1.MsgVote:
+		return m.handleMsgVoteV1Beta1(tx, cosmosMsg)
+
+	case *govtypesv1beta1.MsgDeposit:
+		return m.handleMsgDepositV1Beta1(tx, cosmosMsg)
 
 	case *govtypesv1.MsgDeposit:
 		return m.handleMsgDeposit(tx, cosmosMsg)
@@ -36,8 +42,8 @@ func (m *Module) HandleMsg(index int, msg sdk.Msg, tx *juno.Tx) error {
 	return nil
 }
 
-// handleMsgSubmitProposal allows to properly handle a handleMsgSubmitProposal
-func (m *Module) handleMsgSubmitProposal(tx *juno.Tx, index int, msg *govtypesv1beta1.MsgSubmitProposal) error {
+// handleMsgSubmitProposalV1Beta1 allows to properly handle a handleMsgSubmitProposalV1Beta1
+func (m *Module) handleMsgSubmitProposalV1Beta1(tx *juno.Tx, index int, msg *govtypesv1beta1.MsgSubmitProposal) error {
 	// Get the proposal id
 	event, err := tx.FindEventByType(index, gov.EventTypeSubmitProposal)
 	if err != nil {
@@ -105,6 +111,22 @@ func (m *Module) handleMsgDeposit(tx *juno.Tx, msg *govtypesv1.MsgDeposit) error
 	})
 }
 
+// handleMsgDeposit allows to properly handle a handleMsgDeposit
+func (m *Module) handleMsgDepositV1Beta1(tx *juno.Tx, msg *govtypesv1beta1.MsgDeposit) error {
+	deposit, err := m.source.ProposalDeposit(tx.Height, msg.ProposalId, msg.Depositor)
+	if err != nil {
+		return fmt.Errorf("error while getting proposal deposit: %s", err)
+	}
+	txTimestamp, err := time.Parse(time.RFC3339, tx.Timestamp)
+	if err != nil {
+		return fmt.Errorf("error while parsing time: %s", err)
+	}
+
+	return m.db.SaveDeposits([]types.Deposit{
+		types.NewDeposit(msg.ProposalId, msg.Depositor, deposit.Amount, txTimestamp, tx.Height),
+	})
+}
+
 // handleMsgVote allows to properly handle a handleMsgVote
 func (m *Module) handleMsgVote(tx *juno.Tx, msg *govtypesv1.MsgVote) error {
 	txTimestamp, err := time.Parse(time.RFC3339, tx.Timestamp)
@@ -113,6 +135,18 @@ func (m *Module) handleMsgVote(tx *juno.Tx, msg *govtypesv1.MsgVote) error {
 	}
 
 	vote := types.NewVote(msg.ProposalId, msg.Voter, msg.Option, txTimestamp, tx.Height)
+
+	return m.db.SaveVote(vote)
+}
+
+// handleMsgVoteV1Beta1 allows to properly handle a handleMsgVoteV1Beta1
+func (m *Module) handleMsgVoteV1Beta1(tx *juno.Tx, msg *govtypesv1beta1.MsgVote) error {
+	txTimestamp, err := time.Parse(time.RFC3339, tx.Timestamp)
+	if err != nil {
+		return fmt.Errorf("error while parsing time: %s", err)
+	}
+
+	vote := types.NewVote(msg.ProposalId, msg.Voter, govtypesv1.VoteOption(msg.Option), txTimestamp, tx.Height)
 
 	return m.db.SaveVote(vote)
 }
